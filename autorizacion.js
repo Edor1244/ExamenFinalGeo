ba9f2732806154df9bec968616491bd93138f97f
Eder <guusanito123@gmail.com>
2021-06-17 19:43:22 -0500

    mano

---
+++ autorizacion.js
@@ -0,0 +1,31 @@
+
+const formaingresar = document.getElementById('ingresar');
+const salir = document.getElementById('btnCS');
+
+
+    formaingresar.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let email = formaingresar['email'].value;
+        let password = formaingresar['password'].value;
+
+        auth.signInWithEmailAndPassword(email, password).then(cred => {
+            console.log('Existe el Usuario');
+            formaingresar.reset();
+            window.location = 'Index.html';
+
+        }).catch(err => {
+            alert('Correo o Contraseña Erroneas');
+            console.log(err);
+        });
+    });
+    
+    function CerrarS() {
+        window.location = 'Index2.html';
+    }
+
+
+
+
+
+
+
